e819f1d0da7233c0510c271ec19e83feb2178857
Ghost <duchojakub987@gmail.com>
2025-11-25 11:06:58 +0100

    finally grabbing random word from the list

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,15 @@
 use std::fs;
 use std::error::Error;
+use rand::prelude::IndexedRandom;
 
 pub fn grab_word(file_path: &str) -> Result<String, Box<dyn Error>>{
     let contents = fs::read_to_string(file_path)?;
-    Ok(contents)
+    let lines: Vec<&str> = contents.lines().collect();
+
+    match lines.choose(&mut rand::rng()) {
+        Some(&line) => Ok(line.to_string()),
+        None => Err("File contains no lines".into()),
+    }
 }
 
 pub fn start_game(selected_word: String){
